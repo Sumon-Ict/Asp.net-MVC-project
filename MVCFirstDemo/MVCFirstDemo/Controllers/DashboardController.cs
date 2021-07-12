1f7a8968f03de6df1b3133bb8d9b27e63a5553df
sumon <sumoniceiu1415@gmail.com>
2021-07-12 09:10:15 +0600

    added MVC demo

---
+++ MVCFirstDemo/MVCFirstDemo/Controllers/DashboardController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVCFirstDemo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCFirstDemo.Controllers
+{
+    public class DashboardController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Summary()
+        {
+            //var model = new SummaryModel("sumon",1233);
+            var model = new SummaryModel();  //defaut value displayed
+
+            return View(model);
+
+        }
+    }
+}
---
+++ MVCFirstDemo/MVCFirstDemo/Models/SummaryModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCFirstDemo.Models
+{
+    public class SummaryModel
+    {
+        public string Name { get; set; }
+
+        public int age { get; set; }
+
+        public SummaryModel(string name,int _age)
+        {
+            Name = name;
+            age = _age;
+        }
+
+        public SummaryModel()
+        {
+            Name = "Kalam ";
+            age = 67;
+        }
+    }
+}
