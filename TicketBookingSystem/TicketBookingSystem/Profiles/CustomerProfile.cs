d26ed852dfa392aa8d8baf2b1e39612423da52e8
sumon <sumoniceiu1415@gmail.com>
2021-08-22 09:47:03 +0600

    added CRUD operation done

---
+++ TicketBookingSystem/TicketBookingSystem/Profiles/CustomerProfile.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TicketBookingSystem.Areas.Admin.Models;
+using BO = TicketBookingSystem.Training.BusinessObjects;
+
+namespace TicketBookingSystem.Profiles
+{
+    public class CustomerProfile : Profile
+    {
+        public CustomerProfile()
+        {
+            CreateMap<CreateCustomerModel, BO.Customer>().ReverseMap();
+            CreateMap<EditCustomerModel, BO.Customer>().ReverseMap();
+            CreateMap<CreateTicketModel, BO.Ticket>().ReverseMap();
+            CreateMap<EditTicketModel, BO.Ticket>().ReverseMap();
+
+
+
+        }
+    }
+}
